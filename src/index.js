026b0548f233700973e14ca8cc12e4e0352a7195
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-10-11 15:28:34 +0200

    Search for phone number working but with static method in endpoint

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
-/*
 document.getElementById("searchButton").onclick = function() {
-    console.log("hey")
     var input = document.getElementById("searchInput").value;
-    fetch("http://localhost:8080/jpareststarter/api/person/" + value)
+    fetch("http://localhost:8080/jpareststarter/api/person/" + input)
         .then(response => response.json())
         .then(data => {
-                document.getElementById("searchResult").innerHTML = data.name
+                document.getElementById("searchResult").innerHTML = data.firstName
         }) 
-   
-
-}*/
-
-document.getElementById("searchButton").onclick = function() {
-    console.log("test2")
 }
 
