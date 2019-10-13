e6003cf5306fb05b815c11a344663e784f259bff
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-10-13 14:24:31 +0200

    Revert "Search for phone number working but with static method in endpoint"
    This reverts commit 026b0548f233700973e14ca8cc12e4e0352a7195.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
+/*
 document.getElementById("searchButton").onclick = function() {
+    console.log("hey")
     var input = document.getElementById("searchInput").value;
-    fetch("http://localhost:8080/jpareststarter/api/person/" + input)
+    fetch("http://localhost:8080/jpareststarter/api/person/" + value)
         .then(response => response.json())
         .then(data => {
-                document.getElementById("searchResult").innerHTML = data.firstName
+                document.getElementById("searchResult").innerHTML = data.name
         }) 
+   
+
+}*/
+
+document.getElementById("searchButton").onclick = function() {
+    console.log("test2")
 }
 
