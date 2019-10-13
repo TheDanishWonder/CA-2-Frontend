d9a65f8d41d2821827c41e3ae468aa863bb5be48
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-10-13 17:28:23 +0200

    get person on id works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
+//When pressing search, fetch person from api and make table
 document.getElementById("searchButton").onclick = function() {
     var personId = document.getElementById("searchInput").value;
-    fetch("http://kodebanditterne/CA-2-Backend/api/person/" + personId)
+    fetch("http://localhost:8080/jpareststarter/api/person/" + personId)
         .then(response => response.json())
         .then(data => {
-                document.getElementById("searchResult").innerHTML = data.name
+                document.getElementById("searchResult").innerHTML = makeTableforPerson(data)
         }) 
    
+}
+
+//Make table for found person
+function makeTableforPerson(data) {
+    var table = ("<table><tr><th>Firstname</th><th>Lastname</th><th>Email</th></tr>")
+    table += "<tr><td>" + data.firstName + "</td><td>" + data.lastName + "</td><td>" +
+    data.email + "</td></tr>"
+    table += ("</table>")
+    return table
 }
