a002c2f8398b9c997b9748d156842aca06087239
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-10-13 16:14:01 +0200

    import bundle.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
-/*
 document.getElementById("searchButton").onclick = function() {
-    console.log("hey")
-    var input = document.getElementById("searchInput").value;
-    fetch("http://localhost:8080/jpareststarter/api/person/" + value)
+    var personId = document.getElementById("searchInput").value;
+    fetch("http://kodebanditterne/CA-2-Backend/api/person/" + personId)
         .then(response => response.json())
         .then(data => {
                 document.getElementById("searchResult").innerHTML = data.name
         }) 
    
-
-}*/
-
-document.getElementById("searchButton").onclick = function() {
-    console.log("test2")
 }
-
