2e0c08aec9780c7ced994fd97c09874098871cef
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-10-21 12:31:03 +0200

    get all persons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import { SSL_OP_NETSCAPE_CHALLENGE_BUG } from 'constants';
 
 //When pressing search, fetch person from api and make table
 document.getElementById("searchButton").onclick = function() {
@@ -6,7 +7,7 @@ document.getElementById("searchButton").onclick = function() {
     fetch("https://kodebanditterne.dk/CA-2-Backend/api/person/" + personId)
         .then(response => response.json())
         .then(data => {
-                document.getElementById("searchResult").innerHTML = makeTableforPerson(data)
+            document.getElementById("searchResult").innerHTML = makeTableforPerson(data)
         }) 
    
 }
@@ -19,3 +20,25 @@ function makeTableforPerson(data) {
     table += ("</table>")
     return table
 }
+
+function makeMyTable(array) {
+    function makeTable(Obj) {
+        return "<tr><td>" + Obj.firstName + "</td><td>" + Obj.lastName + 
+               "</td><td>" + Obj.email + "</td><td>" + Obj.phones[0].number + 
+               "</td><td>" + Obj.hobbies + "</td><td>" + Obj.address.street + "</td></tr>\n"
+    }
+    var table = array.map(makeTable)
+    table.unshift("<table><tr><th>Firstname</th><th>Lastname</th><th>Email</th><th>Phone</th><th>Hobbies</th><th>Address</tr>")
+    table.push("</table>")
+    return table
+}
+
+//Get a list of all persons
+document.getElementById("getAllPersonsButton").onclick = function() {
+    fetch("https://kodebanditterne.dk/CA-2-Backend/api/person/all")
+        .then(resonse => resonse.json())
+        .then(data => {
+            document.getElementById("resultAllPersons").innerHTML = makeMyTable(data).join(" ")
+        })
+}
+
