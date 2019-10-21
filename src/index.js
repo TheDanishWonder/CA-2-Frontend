c2fac6ba11673518e8fd64d80cbab7744ebc6f45
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-10-21 10:00:27 +0200

    added number to table

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 //When pressing search, fetch person from api and make table
 document.getElementById("searchButton").onclick = function() {
     var personId = document.getElementById("searchInput").value;
-    fetch("http://localhost:8080/jpareststarter/api/person/" + personId)
+    fetch("https://kodebanditterne.dk/CA-2-Backend/api/person/" + personId)
         .then(response => response.json())
         .then(data => {
                 document.getElementById("searchResult").innerHTML = makeTableforPerson(data)
@@ -13,9 +13,9 @@ document.getElementById("searchButton").onclick = function() {
 
 //Make table for found person
 function makeTableforPerson(data) {
-    var table = ("<table><tr><th>Firstname</th><th>Lastname</th><th>Email</th></tr>")
+    var table = ("<table><tr><th>Firstname</th><th>Lastname</th><th>Email</th><th>Phone</th></tr>")
     table += "<tr><td>" + data.firstName + "</td><td>" + data.lastName + "</td><td>" +
-    data.email + "</td></tr>"
+    data.email + "</td><td>" + data.phones[0].number + "</td></tr>"
     table += ("</table>")
     return table
 }
